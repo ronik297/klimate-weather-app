c40e379f7c2bde665d3709a50e35bc81974d7f63
ronik297 <ronik.allen@gmail.com>
2025-03-24 17:00:33 +0530

    added logos, theme provider (shadcn), header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout";
-import { ThemeProvider } from "./context/theme-provider";
+import { ThemeProvider } from "./context/ThemeProvider";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPage from "./pages/CityPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
